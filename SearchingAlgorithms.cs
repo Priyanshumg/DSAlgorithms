f5f5ef1e2d1592d8ad9653a69f94e8dc90d29bd6
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-08 16:57:03 +0530

    [Priyanshu] Add: Added Functionality to Use Linear Search

--- SearchingAlgorithms.cs
+++ SearchingAlgorithms.cs
@@ -8,5 +8,14 @@ namespace DSAlgo
 {
     internal class SearchingAlgorithms
     {
+        public static int LinearSearch(List<int> DataStructure, int key)
+        {
+            foreach (int element in DataStructure)
+            {
+                if (DataStructure[element] == key)
+                    return element;
+            }
+            return -1;
+        }
     }
 }
