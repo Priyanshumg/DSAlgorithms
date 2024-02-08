a6cb2e8777ae6b96141bf3c1ee1024141bf5ef75
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-08 16:55:47 +0530

    [Priyanshu] Add: Added files for searching

--- Program.cs
+++ Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DSAlgorithm
+namespace DSAlgo
 {
-    internal class SearchingAlgorithms
+    internal class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
