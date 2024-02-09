5e8d795baf4b28b38b46620ba1a1565f64d43b1d
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-09 10:14:20 +0530

    [Priyanshu] Add: Added BubbleSort to SortingAlgorithms

--- SortingAlgorithm.cs
+++ SortingAlgorithm.cs
@@ -8,5 +8,22 @@ namespace DSAlgo
 {
     internal class SortingAlgorithm
     {
+        public void BubbleSort(List<int> DataStructureToSort)
+        {
+            int n = DataStructureToSort.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (DataStructureToSort[j] > DataStructureToSort[j + 1])
+                    {
+                        // Swap DataStructureToSort[j] and DataStructureToSort[j+1]
+                        int temp = DataStructureToSort[j];
+                        DataStructureToSort[j] = DataStructureToSort[j + 1];
+                        DataStructureToSort[j + 1] = temp;
+                    }
+                }
+            }
+        }
     }
 }
