314ad4473bfcb8609abec3f715df67a73a9c10e3
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-09 10:15:46 +0530

    [Priyanshu] Add: Added Selection Sort algorithm to sorting Algorithm.cs

--- SortingAlgorithm.cs
+++ SortingAlgorithm.cs
@@ -25,5 +25,24 @@ namespace DSAlgo
                 }
             }
         }
+        public void SelectionSort(List<int> DataStructureToSort)
+        {
+            int n = DataStructureToSort.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (DataStructureToSort[j] < DataStructureToSort[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                // Swap the found minimum element with the first element
+                int temp = DataStructureToSort[minIndex];
+                DataStructureToSort[minIndex] = DataStructureToSort[i];
+                DataStructureToSort[i] = temp;
+            }
+        }
     }
 }
