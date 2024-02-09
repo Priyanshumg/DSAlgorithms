67abfb2523e6f85a58d5960e247ae98097b75efd
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-09 10:09:06 +0530

    [Priyanshu] Added Functionality for Binary Search

--- SearchingAlgorithms.cs
+++ SearchingAlgorithms.cs
@@ -17,5 +17,29 @@ namespace DSAlgo
             }
             return -1;
         }
+        public static int BinarySearch(List<int> DataStructure, int key)
+        {
+            int left = 0;
+            int right = DataStructure.Count - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                // Check if target is present at mid
+                if (DataStructure[mid] == key)
+                    return mid;
+
+                // If target greater, ignore left half
+                if (DataStructure[mid] < key)
+                    left = mid + 1;
+                // If target is smaller, ignore right half
+                else
+                    right = mid - 1;
+            }
+
+            // If we reach here, then the element was not present
+            return -1;
+        }
     }
 }
